691832d32841cf93ff16e634794ab61588e797d4
AbsElmes <105281207+AbsElmes@users.noreply.github.com>
2026-08-18 13:44:30 +0100

    Update wordpress-developer-toolbox.php

--- wordpress-developer-toolbox.php
+++ wordpress-developer-toolbox.php
@@ -37,7 +37,7 @@ if (! defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('ONION_WORDPRESS_DEVELOPER_TOOLBOX_VERSION', '1.1.6');
+define('ONION_WORDPRESS_DEVELOPER_TOOLBOX_VERSION', '1.1.8');
 define('ONION_WORDPRESS_DEVELOPER_TOOLBOX_NAME', 'onion-wordpress-developer-toolbox');
 define('ONION_WORDPRESS_DEVELOPER_TOOLBOX_NAMESPACE', 'OnionWordpressDeveloperToolbox');
 define('ONION_WORDPRESS_DEVELOPER_TOOLBOX_PLUGIN_FOLDER', __DIR__);
